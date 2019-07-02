54ee6377d241513795b685825589ea79334b7bb6
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-07-02 20:04:15 +0200

    test CORS

--- src/main/java/com/ts/lab/ts/Controllers/DemoControllers/DemoController.java
+++ src/main/java/com/ts/lab/ts/Controllers/DemoControllers/DemoController.java
@@ -9,11 +9,13 @@ public class DemoController {
 
     Student student = new Student(1, "Krzysiek", "M", 123, "mail@mail.com", "Poznan");
 
+    @CrossOrigin
     @GetMapping("")
     public Student demoGet(){
         return student;
     }
 
+    @CrossOrigin
     @PostMapping("")
     public Student demoPost(RequestBody requestBody){
         return (Student) requestBody;
